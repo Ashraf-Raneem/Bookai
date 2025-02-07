820bcee57c5ae1d8170b471541f7b57f19cb6482
Ashraf-Raneem <ashrafraneem@gmail.com>
2025-02-07 01:27:03 -0700

    query added for loading book data

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, jsonify, request
 from flask_session import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
@@ -24,3 +24,44 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return "Project 1: TODO"
+
+
+@app.route("/books", methods=["GET"])
+def get_books():
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    limit = page * per_page
+
+    sql = text("""
+
+    SELECT * FROM books
+    LIMIT :limit
+               
+    """)
+
+    with engine.connect() as conn:
+        res = conn.execute(sql, {'limit': limit})
+        col = res.keys()
+        data = [dict(zip(col, row)) for row in res.fetchall()]
+
+    return jsonify(data)
+
+
+@app.route("/book", methods=["GET"])
+def get_book():
+    book_id = request.args.get('book_id', type=str)
+    print(book_id)
+    sql = text("""
+               
+    SELECT * FROM books
+    WHERE isbn=:book_id
+    
+    """)
+
+    with engine.connect() as conn:
+        res = conn.execute(sql, {'book_id': book_id})
+        col = res.keys()
+        data = [dict(zip(col, row)) for row in res.fetchall()]
+        print(data, res, col)
+
+    return jsonify(data[0])
